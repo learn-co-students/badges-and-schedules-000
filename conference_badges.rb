c29f5f68f1cbaae700d34617caed823b6e4d896c
srz2 <srz222@gmail.com>
2015-05-14 17:11:50 -0400

    Still Working on it

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+$index = 1
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each do |name|
+    badges << badge_maker(name)
+  end
+  badges
+end
+
+def assign_rooms(names)
+  assignments = []
+  names.each do |name|
+    assignments << "Hello, #{name}! You'll be assigned to room #{$index}!"
+    $index += 1
+  end
+  assignments
+end
+
+def printer(input)
+  
+  input.each do |single|
+    puts input
+  end
+end
